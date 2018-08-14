f1247cf2c4f65d2c7af424f54f68c4c11534d1e1
Rongbo Yang <ryang@rmn.com>
2018-08-14 10:42:52 -0500

    Get it build

--- CardIO/CardIO.h
+++ CardIO/CardIO.h
@@ -1,11 +1,21 @@
 //
 //  CardIO.h
-//  Version 5.4.1
+//  CardIO
 //
-//  See the file "LICENSE.md" for the full license governing this code.
+//  Created by Rongbo Yang on 8/14/18.
+//  Copyright © 2018 RetailMeNot. All rights reserved.
 //
 
-// All-in-one header file for card.io sdk.
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardIO.
+FOUNDATION_EXPORT double CardIOVersionNumber;
+
+//! Project version string for CardIO.
+FOUNDATION_EXPORT const unsigned char CardIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardIO/PublicHeader.h>
+
 #import "CardIOCreditCardInfo.h"
 #import "CardIODetectionMode.h"
 #import "CardIOView.h"
@@ -13,4 +23,3 @@
 #import "CardIOPaymentViewController.h"
 #import "CardIOPaymentViewControllerDelegate.h"
 #import "CardIOUtilities.h"
-
